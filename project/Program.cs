a6bfc82d6842d55e52632e49cc9a37d02f87f408
shaharbaba <shaharbaba12@gmail.com>
2021-04-13 15:33:33 +0300

    Progress

--- project/Program.cs
+++ project/Program.cs
@@ -1,26 +1,46 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.IO;
 using System.Net;
 using System.Text;
 
 public class Program
 {
-    public void Main()
+    public static void Main()
     {
         string urlAddress = "https://finance.themarker.com/";
         WriteMessage("Getting the data", ConsoleColor.Green);
         string html = GetHtmlString(urlAddress);
+
+        HtmlDocument doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        HtmlNode TelAviv35 = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div[4]/main/div/div[2]/div[3]/div/div[1]/table/tbody/tr[1]/td[1]");
+        HtmlNode TelAviv35_rate = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div[4]/main/div/div[2]/div[3]/div/div[1]/table/tbody/tr[1]/td[2]");
+        HtmlNode TelAviv35_change = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div[4]/main/div/div[2]/div[3]/div/div[1]/table/tbody/tr[1]/td[3]");
+
         if (html != null)
         {
             WriteMessage("Success!", ConsoleColor.Green);
+            //var output = TelAviv35 != null ? TelAviv35.InnerText : "Error!!";
+            //if (output == "Error!!")
+            //    WriteMessage("Error!!");
+            //else
+            //Console.Write(TelAviv35.InnerText + ": ");
+            Console.Write("Tel Aviv 35: ");
+            Console.Write(TelAviv35_rate.InnerText + " , ");
+            Console.WriteLine(TelAviv35_change.InnerText + "%");
         }
         else
         {
             WriteMessage("Fail", ConsoleColor.Red);
         }
     }
+    //public string FindSpecificWords(string html)
+    //{
 
-    public string GetHtmlString(string url)
+    //}
+
+    public static string GetHtmlString(string url)
     {
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
